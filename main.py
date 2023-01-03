21fa68a4a97ac9387d7fff1106c4e8cf20f9ad1c
Henrique Vieira De Sousa <73469590+Henrique-DeSousa@users.noreply.github.com>
2023-01-03 18:39:34 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,78 +1,45 @@
 from dijkstra import DijkstraSPF, Graph
-import data
-import tabelas
+import topologia_data
 import numpy as np
 
-# cria o dicionário para o dijkstra com base nos caminhos e nas ligações
-
-'''
-combs = []
-for a, y in data.t1_adjacency_list.items():
+# topologia rede c239
+combos = []
+for a, y in topologia_data.c239_adjacent_list.items():
     for z in y:
-        combs.append((a, z))
-edge_weights = dict(zip(combs, data.t1_length))
-print(edge_weights)
-graph = Graph(data.t1_adjacency_list, edge_weights)
-'''
-
-combs = []
-for a, y in data.c239_adjacent_list.items():
-    for z in y:
-        combs.append((a, z))
-edge_weights = dict(zip(combs, data.c239_length))
-graph = Graph(data.c239_adjacent_list, edge_weights)
+        combos.append((a, z))
+edge_weights = dict(zip(combos, topologia_data.c239_length))
+graph = Graph(topologia_data.c239_adjacent_list, edge_weights)
 
 
-# encontra os caminhos mais curtos para qq nos (não tem matriz de trafego)
-
-
-def get_paths(adjacency, graf, matrix=False):
-    """
-    Com base no grafo e numa matrix de trafego que pode ser omitida, gera todos os caminhos possiveis entre os nós.
-    Ordenado pelos caminhos mais curtos primeiro.
-    Remove ainda entradas duplicadas, isto é licação 2->5 é igual a ligação 5->2. Remove a ultima.
-    :param adjacency: (dict) um dicionario com key: nó; value: lista com as suas ligações
-    :param graf: (Objeto Graph) objeto graph do dijkstra
-    :param matrix: (lista de listas) matrix de trafego, pode ser omitida caso nao exista
-    :return spf (list): uma lista com todos os caminhos e a sua distancia,
-            ordenados por ordem crscente de distancia e numero de saltos
-    """
-
-    spf = []
-    if not matrix:  # encontra os caminhos mais curtos para qualquer no
+def generate_paths(adjacency, graf, matrix=False):
+    shortest_path = []
+    if not matrix:
         for i in range(1, len(adjacency.keys()) + 1):
             for j in range(1, len(adjacency.keys()) + 1):
                 dijkstra = DijkstraSPF(graf, i)
-                spf.append((dijkstra.get_path(j), dijkstra.get_distance(j)))
+                shortest_path.append((dijkstra.get_path(j), dijkstra.get_distance(j)))
 
-    else:  # encontra os caminhos mais curtos para qualquer no baseado na matrix
+    else:
         for i, j in enumerate(matrix):
-            for s, k in enumerate(j):  # enumera os valores da linha da matriz
+            for s, k in enumerate(j):
                 if k != 0:
                     dijkstra = DijkstraSPF(graf, i + 1)
-                    spf.append((dijkstra.get_path(s + 1), dijkstra.get_distance(s + 1)))
-    # shortest path first (ordena por distancia, em caso de empate pelo numero de nós
-    # menos nos aparecem primeiro)
-    spf.sort(key=lambda var: (var[1], len(var[0])))
+                    shortest_path.append((dijkstra.get_path(s + 1), dijkstra.get_distance(s + 1)))
 
-    # remove as duplicadas pk esta lib de dijsktra é unidirecional precisamos de meter os dois caminhos ex. (1,
-    # 2) e (2,1) pode dar problemas se o dijkstra nao der o mesmo caminho para 2,5 e 5,2
+    shortest_path.sort(key=lambda var: (var[1], len(var[0])))
 
-    plh = [b for b in spf if b[1] != 0]
+    aux = [b for b in shortest_path if b[1] != 0]
 
-    for j in plh:
+    for j in aux:
         s = j[0][0]
         d = j[0][-1]
-        for i in plh:
+        for i in aux:
             if i[0][0] == d and i[0][-1] == s:
-                plh.remove(i)
-    return plh
+                aux.remove(i)
+    return aux
 
 
-spf_list = get_paths(data.c239_adjacent_list, graph)
-# spf_list = get_paths(data.t1_adjacency_list, graph, data.t1_traffic_matrix)
-# tabelas.tabelas(spf_list, ["path", "km"])
-# tabelas.tabelas(data.t1_traffic_matrix, [i for i in range(1, 7)], [i for i in range(1, 7)])
+shortest_path_list = generate_paths(topologia_data.c239_adjacent_list, graph)
 
 
 def deconstruct(paths):
@@ -83,7 +50,7 @@ def deconstruct(paths):
 
 
 def flipped(key):
-    for i in spf_list:
+    for i in shortest_path_list:
         if list(key) in i:
             return key
     return key[::-1]
@@ -212,6 +179,6 @@ def random(array):
     print("This is the Random:    ", og_dict)
 
 
-first_fit(spf_list)
-most_used(spf_list)
-random(spf_list)
+first_fit(shortest_path_list)
+most_used(shortest_path_list)
+random(shortest_path_list)
